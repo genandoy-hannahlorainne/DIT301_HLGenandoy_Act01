63a72363ee99489003ec76bfb3907a81d5af6fde
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2025-10-21 17:38:10 +0800

    upload first act

--- app/src/main/java/com/example/hellomobiledev/MainActivity.kt
+++ app/src/main/java/com/example/hellomobiledev/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             HelloMobileDevTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Mobile Development - Krislyn Janelle Francisco",
+                        name = "Mobile Development - Hannah Lorainne Genandoy",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
